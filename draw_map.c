1374336141a9870835c030d06ac93d10352886c7
Ansis Maleckis <maleckisansis@gmail.com>
2021-12-20 14:08:44 +0200

    added scaling

--- draw_map.c
+++ draw_map.c
@@ -6,7 +6,7 @@
 /*   By: amalecki <amalecki@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 16:13:48 by amalecki          #+#    #+#             */
-/*   Updated: 2021/12/20 11:35:19 by amalecki         ###   ########.fr       */
+/*   Updated: 2021/12/20 14:07:45 by amalecki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,16 +35,17 @@ int	draw_frame(t_wframe	*wframe)
 		mlx_destroy_image(wframe->window.mlx, wframe->frame.img);
 		wframe->frame.img = mlx_new_image(wframe->window.mlx, 800, 600);
 		i = 0;
-		while(i < wframe->lines -1)
+		while(i < wframe->lines)
 		{
 			j = 0;
-			while (j < wframe->cols - 1)
+			while (j < wframe->cols)
 			{
-				draw_line(&wframe->frame, *(wframe->data[i][j]), *(wframe->data[i + 1][j]));
-				draw_line(&wframe->frame, *(wframe->data[i][j]), *(wframe->data[i][j + 1]));
+				if (i < wframe->lines - 1)
+					draw_line(&wframe->frame, *(wframe->data[i][j]), *(wframe->data[i + 1][j]));
+				if (j < wframe->cols - 1)
+					draw_line(&wframe->frame, *(wframe->data[i][j]), *(wframe->data[i][j + 1]));
 				j++;
 			}
-			draw_line(&wframe->frame, *(wframe->data[i][j]), *(wframe->data[i + 1][j]));
 			i++;
 		}
 	}
@@ -58,17 +59,43 @@ int	mouse_move(int x, int y, t_wframe *wframe)
 		mlx_string_put(wframe->window.mlx, wframe->window.win, x, y, 0xFFFFFF, "Where going");
 }
 
+void	scale(t_points ***data, int lines, int cols, float factor)
+{
+	int	k;
+	int	l;
+
+	printf("hello from scale\n");
+	k = 0;
+	while (k < lines)
+	{
+		l = 0;
+		while (l < cols)
+		{
+			data[k][l]->x *= factor;
+			data[k][l]->y *= factor;
+			data[k][l]->z *= factor;
+			data[k][l]->h *= factor;
+			l++;
+		}
+		k++;
+	}
+}
+
 int	key_hook(int keycode, t_wframe	*wframe)
 {
 	if (keycode == XK_Escape)
 	{
 		wframe->window.mlx->end_loop = True;
 	}
+	else if (keycode == XK_KP_Subtract)
+		scale(wframe->data, wframe->lines, wframe->cols, 0.9);
+	else if (keycode == XK_KP_Add)
+		scale(wframe->data, wframe->lines, wframe->cols, 1.1);
 	printf("keycode: %d\n", keycode);
 	return (0);
 }
 
-int	mouse_hook(int keycode, t_wframe *wframe)
+int	mouse_hook(int keycode, t_wframe wframe)
 {
 	printf("mouse keycode: %d\n", keycode);
 	return (0);
@@ -81,6 +108,7 @@ void	draw_map(t_points ***data, int lines, int columns)
 	wframe.data = data;
 	wframe.lines = lines;
 	wframe.cols = columns;
+	scale(wframe.data, wframe.lines, wframe.cols, 100);
 	wframe.draw_new = true;
 	wframe.window.mlx = mlx_init();
 	wframe.window.win = mlx_new_window(wframe.window.mlx, 1200, 600, "Hello fucker!");
